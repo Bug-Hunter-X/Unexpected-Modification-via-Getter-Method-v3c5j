db6c616de08ccebb7e13144ee3517bb8750d2703
BugHunter-X <askzenai@gmail.com>
2024-12-26 09:06:05 +0100

    Add bug.rb file

---
+++ bug.rb
@@ -0,0 +1,18 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+
+end
+
+my_object = MyClass.new(10)
+puts my_object.value # Output: 10
+
+my_object.value = 20 # This line is the bug
+puts my_object.value # Output: 20 
+```
